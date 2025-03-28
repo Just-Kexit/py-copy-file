5cd12b5c750bf1ccb2ae520c4b73edc44cc92563
Bohdan Mykyta <justkexitmail@gmail.com>
2025-03-28 19:53:41 +0200

    F8ck master again

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(cp: str) -> None:
+
+    file_list = cp.split(" ")
+
+    if len(file_list) != 3 or file_list[0] != "cp":
+        return
+
+    if file_list[1] != file_list[2]:
+        try:
+            with (open(file_list[1], "r") as file_in,
+                open(file_list[2], "w") as file_out):
+                file_out.write(file_in.read())
+        except FileNotFoundError:
+            return
